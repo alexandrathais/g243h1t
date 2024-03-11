1a676e72942061722cef1e349f05bbff26f85ee9
alexandrathais <110062974+alexandrathais@users.noreply.github.com>
2024-03-11 12:30:49 -0300

    Add files via upload

--- js/estudo.js
+++ js/estudo.js
@@ -12,3 +12,32 @@ function sorteia(){
 
     document.getElementById("resultado").innerHTML = sorteio;
 }
+
+function escolher_aba(btn){
+    for(let i = 1; i <= 4; i++){
+        let id_btn = "btn"+i;
+        document.getElementById(id_btn).style.backgroundColor = "aqua";
+        document.getElementById(id_btn).disabled = false;
+        document.getElementById(id_aba).style.display = "none";
+    }
+    if(btn == "1"){
+        document.getElementById("btn1").style.backgroundColor = "red";
+        document.getElementById("btn1").disabled = true;
+        document.getElementById("ab1").style.display = "block";
+    }
+    if(btn == "2"){
+        document.getElementById("btn2").style.backgroundColor = "red";
+        document.getElementById("btn2").disabled = true;
+        document.getElementById("ab2").style.display = "block";
+    }
+    if(btn == "3"){
+        document.getElementById("btn3").style.backgroundColor = "red";
+        document.getElementById("btn3").disabled = true;
+        document.getElementById("ab3").style.display = "block";
+    }
+    if(btn == "4"){
+        document.getElementById("btn4").style.backgroundColor = "red";
+        document.getElementById("btn4").disabled = true;
+        document.getElementById("ab4").style.display = "block";
+    }
+}
